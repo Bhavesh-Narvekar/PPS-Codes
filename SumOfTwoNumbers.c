47bc89901b25b3ca9a196f1a819bf29752b4c061
Bhavesh-Narvekar <bhaveshnarvekar73@gmail.com>
2025-04-20 22:01:40 +0530

    Add files via upload

---
+++ SumOfTwoNumbers.c
@@ -0,0 +1,13 @@
+//Bhavesh Nilesh Narvekar
+//PRN-24030331378016
+//Code-Sum Of Two Number
+//
+#include<stdio.h>
+
+int main()
+{
+    int n1=6,n2=9,sum;
+    sum=n1+n2;
+    printf("%d",sum);
+    return 0;
+}
