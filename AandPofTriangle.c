1756a42ed31360aec6dd514436a5b081b90a32f4
Bhavesh Narvekar <bhaveshnarvekar73@gmail.com>
2025-04-30 01:08:05 +0530

    Add files via upload

---
+++ AandPofTriangle.c
@@ -0,0 +1,19 @@
+//Bhavesh Nilesh Narvekar
+//PRN-24030331378016
+//Code-Division Of Two Number By Taking Input From User
+//Branch-VLSI
+#include<stdio.h>
+int main()
+{
+    int length,breadth,area,perimeter;
+    printf("Enter the Length Of The Rectangle");
+    scanf("%d",&length);
+    printf("Enter the Breadth Of The Rectangle");
+    scanf("%d",&breadth);
+    area=length*breadth;
+    perimeter=2*(length+breadth);
+    printf("The Area Of Rectangle is %d /n he Perimeter Of Rectangle is %d",area,perimeter);
+    return 0;
+
+    
+}
