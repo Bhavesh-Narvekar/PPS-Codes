9781b7d8bb5e25f57a73b1bcc735639f23698b02
Bhavesh Narvekar <bhaveshnarvekar73@gmail.com>
2025-04-30 01:07:47 +0530

    Add files via upload

---
+++ 1To10.c
@@ -0,0 +1,16 @@
+//Bhavesh Nilesh Narvekar
+//PRN-24030331378016
+//Code-Print No From 1 to 10
+//Branch-VLSI
+
+#include <stdio.h>
+int main() 
+{
+    printf("Numbers from 1 to 10:\n");
+    for(int i = 1; i <= 10; i++)
+    {
+        printf("%d ", i);
+    }
+    
+    return 0;
+}
