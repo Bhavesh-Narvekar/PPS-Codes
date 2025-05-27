73cf9720aa160a73075459fbc52d9a408d6d3c66
Bhavesh Narvekar <bhaveshnarvekar73@gmail.com>
2025-05-27 22:34:54 +0530

    Add files via upload

---
+++ Star4x4.c
@@ -0,0 +1,19 @@
+//Bhavesh Nilesh Narvekar
+//PRN-24030331378016
+//Code-4x4 star pattern
+//Branch-VLSI
+
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=0;i<4;i++)
+    {
+        for(j=0;j<4;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
