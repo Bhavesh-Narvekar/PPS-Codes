80a80aa0a1bf77dcd5558ad5a54d8dac013a9345
Bhavesh Narvekar <bhaveshnarvekar73@gmail.com>
2025-06-12 09:27:29 +0530

    Add files via upload

---
+++ FunctionWithoutArgumentWithoutReturnValue.c
@@ -0,0 +1,17 @@
+//Bhavesh Nilesh Narvekar
+//PRN-24030331378016
+//Code-Function Without argument without return value
+//Branch-VLSI
+
+#include<stdio.h>
+int addition()
+{
+    int a=5,b=2,c;
+    c=a+b;
+    printf("Addition %d",c);
+}
+int main()
+{
+    addition();
+    return 0;
+}
