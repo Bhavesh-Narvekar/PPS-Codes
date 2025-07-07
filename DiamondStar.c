d10d363f91a92949e3ad0b22610401c8bfbaf2d4
Bhavesh Narvekar <bhaveshnarvekar73@gmail.com>
2025-07-07 23:34:28 +0530

    Add files via upload

---
+++ DiamondStar.c
@@ -0,0 +1,35 @@
+//Bhavesh Nilesh Narvekar
+//PRN-24030331378016
+//Code-Diamond using Star
+//Branch-VLSI
+#include <stdio.h>
+int main() 
+{
+    int n = 3;
+    int i, j, space;
+    for (i = 1; i <= n; i++) 
+    {
+        for (space = 1; space <= n - i; space++) 
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= i; j++) 
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for (i = n - 1; i >= 1; i--) 
+    {
+        for (space = 1; space <= n - i; space++) 
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= i; j++) 
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
