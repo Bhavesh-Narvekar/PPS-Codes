ba187400916028f4d4c20debf3d6833737cc2d18
Bhavesh Narvekar <bhaveshnarvekar73@gmail.com>
2025-07-07 23:36:16 +0530

    Add files via upload

---
+++ PrintNumber.c
@@ -0,0 +1,15 @@
+//Bhavesh Nilesh Narvekar
+//PRN-24030331378016
+//Code-Print Number 
+//Branch-VLSI
+
+#include <stdio.h>
+
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    printf("You entered: %d\n", number);
+    
+    return 0;
+}
